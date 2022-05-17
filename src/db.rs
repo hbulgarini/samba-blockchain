90f79443e113c996848d3d3f896ffb857d0f8d65
Hector Bulgarini <hbulgarini@gmail.com>
2022-05-17 11:51:00 +0200

    Refactoring (does not work yet)

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,6 @@
 use crate::block::Block;
 use crate::transaction::Transaction;
-
+use serde::{Deserialize, Deserializer, Serialize};
 use std::fs::OpenOptions;
 use std::io::Seek;
 use std::{fs::File, io::Write};
@@ -40,11 +40,30 @@ impl DBConnection {
         }
     }
 
-    fn write_to_db(&mut self, encoded: &[u8]) {
+    /*     fn write_to_db(&mut self, encoded: &[u8]) {
         self.db_file
             .seek(std::io::SeekFrom::Start(0))
             .expect("can't rewind the cursor");
         self.db_file.write_all(encoded).unwrap();
+    } */
+
+    pub fn open<T>(&mut self) -> (Vec<T>, usize) {
+        if self.new {
+            let empty: Vec<T> = Vec::new();
+            return (empty, 0);
+        } else {
+            let items: Vec<T> = bincode::deserialize_from(&self.db_file).unwrap();
+            let current_id = items.len();
+            return (items, current_id);
+        };
+    }
+
+    pub fn write_to_db<T>(&mut self, registry: &Vec<T>) {
+        let encoded: Vec<u8> = bincode::serialize(&registry).unwrap();
+        self.db_file
+            .seek(std::io::SeekFrom::Start(0))
+            .expect("can't rewind the cursor");
+        self.db_file.write_all(&encoded).unwrap();
     }
 }
 
@@ -57,37 +76,5 @@ impl SambaDB {
         println!("{:?}", txs);
 
         SambaDB { blockchain, txs }
-    }
-
-    pub fn open_txs(&mut self) -> (Vec<Transaction>, usize) {
-        if self.txs.new {
-            let empty: Vec<Transaction> = Vec::new();
-            return (empty, 0);
-        } else {
-            let txs: Vec<Transaction> = bincode::deserialize_from(&self.txs.db_file).unwrap();
-            let current_id = txs.len();
-            return (txs, current_id);
-        };
-    }
-
-    fn open_blockchain(&mut self) -> (Vec<Block>, usize) {
-        if self.txs.new {
-            let empty: Vec<Block> = Vec::new();
-            return (empty, 0);
-        } else {
-            let txs: Vec<Block> = bincode::deserialize_from(&self.txs.db_file).unwrap();
-            let current_id = txs.len();
-            return (txs, current_id);
-        };
-    }
-
-    pub fn write_to_blockchain_db(&mut self, registry: &Vec<Block>) {
-        let encoded: Vec<u8> = bincode::serialize(&registry).unwrap();
-        self.blockchain.write_to_db(&encoded);
-    }
-
-    pub fn write_to_txs_db(&mut self, registry: &Vec<Transaction>) {
-        let encoded: Vec<u8> = bincode::serialize(&registry).unwrap();
-        self.txs.write_to_db(&encoded);
     }
 }
